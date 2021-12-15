ffde39d20267806bab417241f9bdf43119ce5e3d
vhf1030 <vhf1030@naver.com>
2021-12-15 18:34:51 +0900

    db 연동 및 google chart 연동 완료

--- coinanser/models.py
+++ coinanser/models.py
@@ -36,16 +36,34 @@ class Comment(models.Model):
     answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
 
 
-class RawData(models.Model):  # error - 테이블이 제대로 생성되지 않음 / 기존 DB를 복사하는 방식으로 진행해야 할 듯
-    date_time = models.DateTimeField(primary_key=True),
-    date_time_last = models.DateTimeField(null=True, blank=True),
-    opening_price = models.FloatField(null=True, blank=True),
-    high_price = models.FloatField(null=True, blank=True),
-    low_price = models.FloatField(null=True, blank=True),
-    trade_price = models.FloatField(null=True, blank=True),
-    candle_acc_trade_price = models.FloatField(null=True, blank=True),
-    candle_acc_trade_volume = models.FloatField(null=True, blank=True)
+# class RawData(models.Model):  # error - 테이블이 제대로 생성되지 않음 / 기존 DB를 복사하는 방식으로 진행해야 할 듯
+#     date_time = models.DateTimeField(primary_key=True),
+#     date_time_last = models.DateTimeField(null=True, blank=True),
+#     opening_price = models.FloatField(null=True, blank=True),
+#     high_price = models.FloatField(null=True, blank=True),
+#     low_price = models.FloatField(null=True, blank=True),
+#     trade_price = models.FloatField(null=True, blank=True),
+#     candle_acc_trade_price = models.FloatField(null=True, blank=True),
+#     candle_acc_trade_volume = models.FloatField(null=True, blank=True)
 
+# 기존 db와 연동하는 경우:
+# 1. python manage.py inspectdb 로 확인 및 복사하여 class 생성
+# 2. python manage.py makemigrations
+# 3. python manage.py migrate / 또는 python manage.py migrate --fake
+
+class RawdataKrwAda(models.Model):
+    date_time = models.DateTimeField(primary_key=True)  # primary_key가 db에 설정이 안되어있는 경우 직접 입력해야 함
+    date_time_last = models.DateTimeField(blank=True, null=True)
+    opening_price = models.FloatField(blank=True, null=True)
+    high_price = models.FloatField(blank=True, null=True)
+    low_price = models.FloatField(blank=True, null=True)
+    trade_price = models.FloatField(blank=True, null=True)
+    candle_acc_trade_price = models.FloatField(blank=True, null=True)
+    candle_acc_trade_volume = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'rawdata_krw-ada'
 
 
 
--- coinanser/views/rawdata_views.py
+++ coinanser/views/rawdata_views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from coinanser.models import RawData
+from coinanser.models import RawdataKrwAda
 # import json
 
-
 def select_test(request):
+    # rawdata = RawdataKrwAda.objects.order_by('-date_time')[0:50]
+    rawdata = RawdataKrwAda.objects.all()[0:200]
+    test = RawdataKrwAda.objects.all()[0:10]
     context = {
-        'data_set': [
-            [1,  37.8, 80.8, 41.8],
-            [2, 30.9, 69.5, 32.4],
-            [3, 25.4, 57, 25.7],
-            [4, 11.7, 18.8, 10.5],
-            [5, 11.9, 17.6, 10.4],
-            [6, 11.9, 17.6, 100.4],
-            [7, 7.6, 12.3, 9.6],
-            [8, 12.3, 29.2, 10.6],
-            [9, 16.9, 42.9, 14.8],
-            [10, 12.8, 30.9, 11.6],
-            [11, 5.3, 7.9, 4.7],
-            [12, 6.6, 8.4, 5.2],
-            [13, 4.8, 6.3, 3.6],
-            [14, 4.2, 6.2, 3.4],
-        ]
+        # 'data_set': [
+        #     [1,  37.8, 80.8, 41.8],
+        #     [2, 30.9, 69.5, 32.4],
+        #     [3, 25.4, 57, 25.7],
+        #     [4, 11.7, 18.8, 10.5],
+        # ],
+        'rawdata': rawdata,
+        'test': test,
     }
     # return render(request, 'google_chart/linechart_material.html', {'contextJson': json.dumps(context)})
     return render(request, 'google_chart/linechart_material.html', context)
