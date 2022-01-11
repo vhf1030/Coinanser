cc344b71487dba7198826e8f30d84fcec9e7428f
vhf1030 <vhf1030@naver.com>
2022-01-11 11:43:17 +0900

    첫 번째 예측모델 배포 완료

--- coinanser/upbit_api/utils.py
+++ coinanser/upbit_api/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from pprint import pprint
+import math
 
 
 def datetime_convert(time_, to_str=True, to_utc=False, sec_delta=0, month_delta=0):
@@ -29,4 +30,35 @@ def datetime_convert(time_, to_str=True, to_utc=False, sec_delta=0, month_delta=
         return str_time
 
     return dtime
+
+
+UPBIT_PRICE_UNIT = {
+    0: 0.01,
+    10: 0.1,
+    100: 1,
+    1000: 5,
+    10000: 10,
+    100000: 50,
+    500000: 100,
+    1000000: 500,
+    2000000: 1000,
+}
+
+
+def ceil_unit(float_):
+    for p in UPBIT_PRICE_UNIT:
+        if float_ > p:
+            upu = UPBIT_PRICE_UNIT[p]
+        else:
+            continue
+    result = math.ceil(float_/upu) * upu
+    result = int(result*100000000)/100000000
+    return result
+
+
+def file_version_parser(path_):
+    file_name = '.'.join(path_.split('/')[-1].split('.')[:-1])
+    file, version = '_'.join(file_name.split('_')[:-1]), float(file_name.split('_')[-1])
+    return file, version
+
 
